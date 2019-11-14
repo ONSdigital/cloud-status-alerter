45452d95a378f4beb15df99aa22ddc3589b78402
John Topley <john.topley@ons.gov.uk>
2019-11-14 15:48:02 +0000

    Format <b> elements as bold within the Slack message

--- provider.rb
+++ provider.rb
@@ -6,6 +6,7 @@ require 'rest-client'
 # Base class that defines a common interface for all providers.
 class Provider
   WHITESPACE_ELEMENTS = {
+    'b' => { before: '*', after: '*' },
     'br' => { before: "\n", after: '' },
     'p' => { before: "\n", after: "\n" },
     'strong' => { before: '*', after: '*' }
