f710d114965b11404c6d52e91b5c02ca5ad9dc38
John Topley <john.topley@ons.gov.uk>
2019-11-14 12:06:53 +0000

    Added GitHub provider

--- provider.rb
+++ provider.rb
@@ -5,6 +5,12 @@ require 'rest-client'
 
 # Base class that defines a common interface for all providers.
 class Provider
+  WHITESPACE_ELEMENTS = {
+    'br' => { before: "\n", after: '' },
+    'p' => { before: "\n", after: "\n" },
+    'strong' => { before: '*', after: '*' }
+  }.freeze
+
   attr_reader :icon, :name
 
   StatusFeedUpdate = Struct.new(:id, :timestamp, :metadata, :text, :uri, keyword_init: true)
