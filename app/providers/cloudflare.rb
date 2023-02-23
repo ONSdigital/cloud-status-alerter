9f98bf025acf5408f651a83f88638c423ae3927a
rhiGriff <rhiannon.griffiths@ons.gov.uk>
2023-02-23 08:19:30 +0000

    add cloudflare

---
+++ app/providers/cloudflare.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'feedjira'
+require 'sanitize'
+require_relative '../provider'
+
+# Provider for Cloudflare.
+class Cloudflare < Provider
+  STATUS_FEED_URL = 'https://www.cloudflarestatus.com/history.atom'
+
+  def initialize
+    super
+    @icon = 'cloudflare'
+    @name = 'Cloudflare'
+  end
+
+  def latest_update
+    feed = Feedjira.parse(RestClient.get(STATUS_FEED_URL))
+    raise "Unable to retrieve RSS feed from #{STATUS_FEED_URL}" unless feed
+    return nil if feed.entries.empty?
+
+    latest = feed.entries.first
+    StatusFeedUpdate.new(id: latest.entry_id,
+                         timestamp: latest.published.to_datetime.to_s,
+                         metadata: latest.title,
+                         text: Sanitize.fragment(latest.summary, whitespace_elements: WHITESPACE_ELEMENTS),
+                         uri: latest.url)
+  end
+end
